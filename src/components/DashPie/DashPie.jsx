a8ab406ff8127271821f8a127a14f6d992b84d81
camilla alves <camillaalves285@gmail.com>
2023-07-18 16:39:31 -0300

    Adicionei os gráficos da primeira página

---
+++ src/components/DashPie/DashPie.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import S from './styles.module.scss';
+
+export function DashPie() {
+  
+  const series = [24.05, 73, 41, 50, 95];
+  const names = ['Maria', 'João', 'Ana', 'Camilla', 'Barbara']
+  const labels = series.map((value, index) => `Dr. ${names[index]} - <b>R$ ${value}</b>`);
+  
+  const options = {
+    labels: labels,
+    chart: {
+      type: 'polarArea',
+    },
+    tooltip: {
+      y: {
+        formatter: function (value, { seriesIndex, dataPointIndex, w }) {
+          return '';
+        }
+      }
+    },
+    stroke: {
+      colors: ['#fff']
+    },
+    fill: {
+      opacity: 0.8
+    },
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: '200px'
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  };
+
+  return (
+    <div className={S.container}>
+      <div id="chart" className={S.dashPie}>
+        <ReactApexChart options={options} series={series} type="polarArea" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/DashSpline/DashSpline.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import S from './styles.module.scss'
+
+export function DashSpline() {
+
+     const series= [{
+      name: 'Dr. Paulo',
+      data: [3110, 2140, 1228, 1551, 1042, 709, 2810]
+    }, {
+      name: 'Dr. Miriam',
+      data: [2511, 1232, 1145, 1032, 2034, 1052, 2641]
+    }, {
+      name: 'Dr. Yasmim',
+      data: [1050, 3245, 2022, 2332, 1543, 1272, 2141]
+    }
+  
+  ]
+
+    const options = {
+      chart: {
+        height: 350,
+        type: 'area',
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth'
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: ["2018-09-19", "2018-09-18", "2018-09-17", "2018-09-16", "2018-09-15", "2018-09-14", "2018-09-13"]
+      },
+      tooltip: {
+        x: {
+          format: 'dd/MM/yy'
+        },
+      }
+   }
+   return(
+    <div className={S.container}>
+      <div className={S.dashSpline}>
+         <ReactApexChart options={options} series={series}  type='area'/>
+      </div>
+    </div>
+   )
+   }
