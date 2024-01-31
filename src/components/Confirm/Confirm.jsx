32b5e958602dc1d1fba7e398e2bafcf31f4145f6
camilla <camillaalves285@gmail.com>
2024-01-31 15:15:30 -0300

    a

---
+++ src/components/Confirm/Confirm.jsx
@@ -0,0 +1,27 @@
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+export function Confirm(props) {
+   return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Cadastro feito
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6>
+            {props.description}
+          </h6>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button color='dark' onClick={props.onHide}>Fechar</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+}
