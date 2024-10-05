542f456b7712e3ade61b9bad5a4dffcaa3d2b77a
camilla <camillaalves285@gmail.com>
2024-10-05 17:26:08 -0300

    Ainda está com o bug do segundo gráfico sem renderizar

--- client/src/components/DashSpline/DashSpline.jsx
+++ client/src/components/DashSpline/DashSpline.jsx
@@ -11,64 +11,54 @@ export function DashSpline() {
   useEffect(() => {
     fetchMonthlyDataForDay();
   }, []);
-
+  
   const fetchMonthlyDataForDay = async () => {
     const currentDate = new Date();
     const year = currentDate.getFullYear();
     const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  
+    const formattedDate = `${year}-${month}`; // Use o primeiro dia do mês para garantir que há dados
 
-    const formattedDate = `${year}-${month}`;
-
-    const dataToSend = {
-      data: formattedDate,
-    };
-
+    const dataToSend = { data: formattedDate };
+  
     const getClinicId = () => {
       const userDataString = localStorage.getItem("user");
       const userData = JSON.parse(userDataString);
       const clinicId = userData?.user?.clinicaId;
       return clinicId;
     };
-
+  
     try {
       const response = await api.put(`/dailyData/${getClinicId()}`, dataToSend);
-
-      if (
-        response.data.detalhes_por_dia &&
-        Array.isArray(response.data.detalhes_por_dia)
-      ) {
-        const extractedDays = response.data.detalhes_por_dia.map(
-          (item) => item.dia
-        );
+  
+      console.log(response.data); // Verificar estrutura completa da resposta
+  
+      if (response.data.detalhes_por_dia && Array.isArray(response.data.detalhes_por_dia)) {
+        const extractedDays = response.data.detalhes_por_dia.map((item) => item.dia);
         setDaysOnly(extractedDays);
-
-        const updatedSeries = response.data.detalhes_por_dia.reduce(
-          (acc, item) => {
-            if (Array.isArray(item.profissionais)) {
-              item.profissionais.forEach((professional) => {
-                const existingSeriesIndex = acc.findIndex(
-                  (serie) => serie.name === professional.profissional
-                );
-                if (existingSeriesIndex !== -1) {
-                  const existingSeries = acc[existingSeriesIndex];
-                  existingSeries.data[extractedDays.indexOf(item.dia)] =
-                    professional.totalValue;
-                } else {
-                  const newData = new Array(extractedDays.length).fill(0);
-                  newData[extractedDays.indexOf(item.dia)] =
-                    professional.totalValue;
-                  acc.push({
-                    name: professional.profissional,
-                    data: newData,
-                  });
-                }
-              });
-            }
-            return acc;
-          },
-          []
-        );
-
+  
+        const updatedSeries = response.data.detalhes_por_dia.reduce((acc, item) => {
+          if (Array.isArray(item.profissionais)) {
+            item.profissionais.forEach((professional) => {
+              const existingSeriesIndex = acc.findIndex(
+                (serie) => serie.name === professional.profissional
+              );
+              if (existingSeriesIndex !== -1) {
+                const existingSeries = acc[existingSeriesIndex];
+                existingSeries.data[extractedDays.indexOf(item.dia)] = professional.totalValue;
+              } else {
+                const newData = new Array(extractedDays.length).fill(0);
+                newData[extractedDays.indexOf(item.dia)] = professional.totalValue;
+                acc.push({
+                  name: professional.profissional,
+                  data: newData,
+                });
+              }
+            });
+          }
+          return acc;
+        }, []);
+  
         setSeries(updatedSeries);
       } else {
         console.log("detalhes_por_dia está vazio ou indefinido");
@@ -77,6 +67,7 @@ export function DashSpline() {
       console.error("Erro ao buscar dados:", error); // Mensagem de erro amigável
     }
   };
+  
 
   if (series.length === 0) {
     return <Refresh title="Carregando" />;
