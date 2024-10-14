6c7bce9ea7c034fc2667abc8d91e010fbf53d2e7
camilla <camillaalves285@gmail.com>
2024-10-14 14:43:22 -0300

    Adicionei novos cabeçalhos nas requisições

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,21 @@
-// import express from 'express'
-// import { router } from './routes'
-// import cors from 'cors'
-
-// const app = express()
-
-// app.use(cors())
-// app.use(express.json())
-// app.use(router)
-
-// app.listen(3030, () => console.log('🚀 Server listening'))
-
 import express from 'express';
 import { router } from './routes';
 import cors from 'cors';
+// import { verifyToken } from './middlewares/authMiddleware'; 
 
 const app = express();
 
 // Configurando CORS para aceitar requisições apenas do frontend no localhost:5173
 app.use(cors({
   origin: 'http://localhost:5173', // Permita o frontend fazer requisições
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Defina os métodos HTTP permitidos
-  allowedHeaders: ['Authorization', 'Content-Type'], // Permita cabeçalhos específicos
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Authorization', 'Content-Type', 'Cache-Control'],
+  exposedHeaders: ['Authorization']
 }));
 
+
 app.use(express.json());
+// app.use(verifyToken); 
 app.use(router);
 
 app.listen(3030, () => console.log('🚀 Server listening on port 3030'));
