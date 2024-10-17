c467f6e9cfa734c1dba115a3bc7be0946489603e
camilla <camillaalves285@gmail.com>
2024-10-17 15:34:47 -0300

    Modifiquei o nome componente Confirm para Negative

--- client/src/components/Negative/Negative.jsx
+++ client/src/components/Negative/Negative.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import Modal from "react-bootstrap/Modal";
 
-export function Confirm(props) {
+export function Negative(props) {
   return (
     <Modal
       {...props}
